8a58c8eed61ad9e7f528d1e93521d36ab2c16e89
leandroffdev <leandrofrancagti@gmail.com>
2024-07-10 15:10:26 -0300

    corrigido envio de emails

--- js/main.js
+++ js/main.js
@@ -6,20 +6,20 @@ function toggleMenu() {
     }
 }
 
-var params = {
-        from_name: document.getElementById('name').value,
-        email_id: document.getElementById('email').value,
-        subject: document.getElementById('subject').value,
-        message: document.getElementById('message').value
-    }
+
 // Send email data
 function sendEmail() {
-    console.log('enviado');
+    console.log('enviado')
+    var params = {
+        from_name: document.getElementById('name').value,
+        email_id: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    }
     emailjs.send("service_dajc34g","template_p01dl9g", params).then((response) => {
         console.log('SUCCESS!', response.status, response.text);
         },
         (error) => {
         console.log('FAILED...', error);
-        },
+        }
     )
 }
